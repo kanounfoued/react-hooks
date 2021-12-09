b745c5653f47657556d3abca790951ba82c785b2
kanounfoued <kanoun-foued@outlook.com>
2021-12-09 21:09:52 +0100

    :hammer: :fire: remove unused function

--- src/Hooks/useNextPrevious/index.js
+++ src/Hooks/useNextPrevious/index.js
@@ -19,23 +19,6 @@ export default function useNextPrevious({ items = [], index = 0 }) {
   }, [index, items]);
 
   /**
-   * Search for an element inside a list
-   * @param {*} itemId
-   * @param {*} callback passing a callback, to make the search customizable dependent on your data
-   */
-  function findItemIndex(itemId = "", callback = null) {
-    if (isItemsEmpty()) {
-      return -1;
-    }
-
-    if (callback) {
-      return items.findIndex(callback);
-    }
-
-    return items.findIndex((item) => item._id === itemId);
-  }
-
-  /**
    * Check if the list of items is empty
    */
   function isItemsEmpty() {
@@ -81,7 +64,6 @@ export default function useNextPrevious({ items = [], index = 0 }) {
   return {
     getNext,
     getPrevious,
-    findItemIndex,
     disableNext,
     disablePrevious,
   };
