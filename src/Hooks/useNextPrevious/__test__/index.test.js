727a04b91476acc1e5e85bb9ee59744acdf1b2aa
kanounfoued <kanoun-foued@outlook.com>
2021-12-13 10:21:04 +0100

    :white_check_mark: add tests for disableNext, disablePrevious and getNext function

--- src/Hooks/useNextPrevious/__test__/index.test.js
+++ src/Hooks/useNextPrevious/__test__/index.test.js
@@ -1,37 +1,120 @@
 import useNextPrevious from "../";
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 
-let items = [];
+describe("Test disable properties", function () {
+  test("Test disable properties useNextPrevious with empty data", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [], index: 0 })
+    );
 
-// beforeEach(() => {
-//   items.push({});
-// });
+    expect(result.current.disableNext).toBe(true);
+    expect(result.current.disablePrevious).toBe(true);
+  });
 
-afterEach(() => {
-  items.push({});
+  test("Test disable properties useNextPrevious with one element", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}], index: 0 })
+    );
+
+    expect(result.current.disableNext).toBe(true);
+    expect(result.current.disablePrevious).toBe(true);
+  });
+
+  test("Test disable properties useNextPrevious with two and more elements", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}, {}], index: 0 })
+    );
+
+    expect(result.current.disableNext).toBe(false);
+    expect(result.current.disablePrevious).toBe(true);
+  });
+
+  test("Test disable properties useNextPrevious with two and more elements, and index greater than items length", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}, {}], index: 5 })
+    );
+
+    expect(result.current.disableNext).toBe(true);
+    expect(result.current.disablePrevious).toBe(true);
+  });
+
+  test("Test disable properties useNextPrevious with two and more elements, and index less than 0", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}, {}, {}], index: -1 })
+    );
+
+    expect(result.current.disableNext).toBe(true);
+    expect(result.current.disablePrevious).toBe(true);
+  });
+
+  test("Test disable properties useNextPrevious with 3 elements, and index === 1", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}, {}, {}], index: 1 })
+    );
+
+    expect(result.current.disableNext).toBe(false);
+    expect(result.current.disablePrevious).toBe(false);
+  });
+
+  test("Test disable properties useNextPrevious with 3 elements, and index === 2", () => {
+    const { result } = renderHook(() =>
+      useNextPrevious({ items: [{}, {}, {}], index: 2 })
+    );
+
+    expect(result.current.disableNext).toBe(true);
+    expect(result.current.disablePrevious).toBe(false);
+  });
 });
 
-afterAll(() => {
-  items = [];
-});
+describe("Test getNext() function", function () {
+  test("Test getNext() with 3 elements, and index 0", function () {
+    const items = [{}, {}, {}];
+    const index = 0;
 
-test("should useNextPrevious with empty data", () => {
-  const { result } = renderHook(() => useNextPrevious({ items, index: 0 }));
+    const { result } = renderHook(() => useNextPrevious({ items, index }));
 
-  expect(result.current.disableNext).toBe(true);
-  expect(result.current.disablePrevious).toBe(true);
-});
+    const { getNext } = result.current;
 
-test("should useNextPrevious with one element", () => {
-  const { result } = renderHook(() => useNextPrevious({ items, index: 0 }));
+    let res = null;
 
-  expect(result.current.disableNext).toBe(true);
-  expect(result.current.disablePrevious).toBe(true);
-});
+    act(() => {
+      res = getNext();
+    });
 
-test("should useNextPrevious with two and more elements", () => {
-  const { result } = renderHook(() => useNextPrevious({ items, index: 0 }));
+    expect(res).toBe(items[index + 1]);
+  });
 
-  expect(result.current.disableNext).toBe(false);
-  expect(result.current.disablePrevious).toBe(true);
+  test("Test getNext() with 3 elements, and index 1", function () {
+    const items = [{}, {}, {}];
+    const index = 1;
+
+    const { result } = renderHook(() => useNextPrevious({ items, index }));
+
+    const { getNext } = result.current;
+
+    let res = null;
+
+    act(() => {
+      res = getNext();
+    });
+
+    expect(res).toBe(items[index + 1]);
+  });
+
+  test("Test getNext() with 3 elements, and index 2", function () {
+    const items = [{}, {}, {}];
+    const index = 2;
+
+    const { result } = renderHook(() => useNextPrevious({ items, index }));
+
+    const { getNext } = result.current;
+
+    let res = null;
+
+    act(() => {
+      res = getNext();
+    });
+
+    expect(res).toBe(null);
+  });
 });
